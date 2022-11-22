438c67f17d622a9716203f83161d3bd00bb99835
Aleksei <Ky34.aleksei@gmail.com>
2022-11-22 22:02:08 +0300

    fix

--- 9. Immediately Invoked Function Expression (IIFE)/script.js
+++ 9. Immediately Invoked Function Expression (IIFE)/script.js
@@ -1,1 +1,26 @@
 'use strict';
+
+// IIFE (функции экспрешн вызываемые немедленно)
+
+const runOneTime = function () {
+  console.log('You will never see this function call again');
+};
+// runOneTime();
+
+// вся функция помещается в скобки и ставятся скобки в конце
+(function () {
+  console.log('You will never see this function call again');
+  const x = 1; // все данные которые находятся во внутреннем scope функции они приванты, это называется инкапсуляция, переменная х инкапсулированна в scopе этой функции
+  var z = 3;
+})(); // IIFE патерн использовался для создания приватного scope для функций, объявляемых ключевым словом var
+
+// В СОВРЕМЕННОМ JS ПАТЕРН IIFE ИСПОЛЬЗУЕТСЯ ДЛЯ СОЗДАНИЯ ФУНКЦИЙ, КОТОРЫЕ НУЖНО ИСПОЛЬЗОВАТЬ ТОЛЬКО 1 РАЗ
+
+(() => console.log('You will never see this ARROW function call again'))();
+
+{
+  // const x = 1;
+  var y = 2;
+}
+
+console.log(z);
